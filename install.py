9ae50a44931aeebb1228bd7186a441c2b342738b
radiabyte <72905706+radiabyte@users.noreply.github.com>
2023-12-07 09:43:25 -0500

    Update install.py

--- install.py
+++ install.py
@@ -1,4 +1,6 @@
 import os
 os.system('pip install pillow')
 os.system('pip install customtkinter')
+os.system('pip install packaging')
+os.system('pip install requests')
 os.system('pip install ./pytube-15.0.0-py3-none-any.whl')
